fa3ac2b50e981349d39df1b5422dc1f215450306
samikshya <samikshyakharel405@gmail.com>
2024-05-22 13:21:56 +1000

    todolist craeted and added function like edit delete and done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputField from "./component/InputField";
+import { Todo } from "./component/model";
+import ToDoList from "./component/ToDoList";
 
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  // console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("")
+    }
+    console.log(todos);
+    
+  };
 
-const App:React.FC = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <span className="heading">Taskify</span>
 
-export default App
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    <ToDoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
+
+export default App;
